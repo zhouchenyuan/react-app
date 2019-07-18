3ad0c827182aa19b0169238ad7afbd2579a9ab8c
zhouchenyuan <2807372569@qq.com>
2019-07-18 16:01:22 +0800

    已用ui库

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+          <Button type="primary">Button</Button>
       </header>
     </div>
   );
